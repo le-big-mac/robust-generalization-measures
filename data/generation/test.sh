140f7e0ca616213fcfab1744a6ac7d5442659e7e
Charlie <charlielondon02@gmail.com>
2021-04-16 04:01:37 +0100

    test slurm script

---
+++ data/generation/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=10:00:00
+
+# set name of job
+#SBATCH --job-name=lr_test
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=charles.london@wolfson.ox.ac.uk
+
+# run the application
+lrs='0.001 0.00158 0.00316 0.00631 0.01'
+
+for lr in $lrs; do
+  python3 train.py --log_epoch_freq=5 --lr=$lr
+done
