c9432041b0b4011c87bb27117b3d5892c10a1a35
Charlie <charlielondon02@gmail.com>
2021-05-01 20:56:18 +0100

    shell scripts, kendall coeff

--- data/generation/single_model.sh
+++ data/generation/single_model.sh
@@ -18,9 +18,4 @@
 # send mail to this address
 #SBATCH --mail-user=charles.london@wolfson.ox.ac.uk
 
-# run the application
-lrs='0.001 0.00158 0.00316 0.00631 0.01'
-
-for lr in $lrs; do
-  python3 train.py --log_epoch_freq=5 --lr=$lr
-done
+python3 train.py --log_epoch_freq=5 --lr="$1" --depth="$2" --batch_size="$3" --weight_decay="$4" --batch_norm="$5"
