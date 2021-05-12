7f4d77a8c0a7ac84c1514cc2f143fd91d3a7c280
Charlie <charlielondon02@gmail.com>
2021-05-12 21:18:16 +0100

    job loops

--- data/generation/job_loop.sh
+++ data/generation/job_loop.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # run the application
-lrs='0.001 0.00158 0.00316 0.00631 0.01'
-depth='2 3 4 5 6'
+depth='2 3 4 5'
 batch_size='32 64 128 256'
 weight_decay='0'
 batch_norm='True'
 dropout_prob='0'
 
-for lr in $lrs; do
-  for d in $depth; do
-    for b in $batch_size; do
-      for w in $weight_decay; do
-        for bn in $batch_norm; do
-          for p in $dropout_prob; do
-            sbatch -p small single_model.sh "$lr" "$d" "$b" "$w" "$bn" "$p"
-          done
+for d in $depth; do
+  for b in $batch_size; do
+    for w in $weight_decay; do
+      for bn in $batch_norm; do
+        for p in $dropout_prob; do
+          sbatch -p small single_model.sh "$d" "$b" "$w" "$bn" "$p"
         done
       done
     done
--- data/generation/single_model.sh
+++ data/generation/single_model.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 
 # set max wallclock time
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 
 # set name of job
 #SBATCH --job-name=lr_test
@@ -19,5 +19,8 @@
 #SBATCH --mail-user=charles.london@wolfson.ox.ac.uk
 
 # run the application
+lrs='0.001 0.00158 0.00316 0.00631 0.01'
 
-python3 train.py --log_epoch_freq=5 --lr="$1" --model_depth="$2" --batch_size="$3" --weight_decay="$4" --batch_norm="$5" --dropout_prob="$6"
+for lr in $lrs; do
+  python3 train.py --log_epoch_freq=5 --lr="$lr" --model_depth="$2" --batch_size="$3" --weight_decay="$4" --batch_norm="$5" --dropout_prob="$6"
+done
