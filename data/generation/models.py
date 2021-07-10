604b75a4d3a0332fb590becfaeb55607577f2191
Charlie <charlielondon02@gmail.com>
2021-07-10 23:09:23 +0100

    dropout and batch norm separate

--- data/generation/models.py
+++ data/generation/models.py
@@ -19,26 +19,32 @@ class NiNBlock(nn.Module):
         self.relu = nn.ReLU(inplace=True)
 
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=2, padding=1)
-        self.bn1 = nn.BatchNorm2d(planes) if batch_norm else nn.Dropout2d(p=dropout_prob)
+        self.bn1 = nn.BatchNorm2d(planes) if batch_norm else lambda x: x
+        self.dp1 = nn.Dropout2d(p=dropout_prob)
 
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=1, stride=1)
-        self.bn2 = nn.BatchNorm2d(planes) if batch_norm else nn.Dropout2d(p=dropout_prob)
+        self.bn2 = nn.BatchNorm2d(planes) if batch_norm else lambda x: x
+        self.dp2 = nn.Dropout2d(p=dropout_prob)
 
         self.conv3 = nn.Conv2d(planes, planes, kernel_size=1, stride=1)
-        self.bn3 = nn.BatchNorm2d(planes) if batch_norm else nn.Dropout2d(p=dropout_prob)
+        self.bn3 = nn.BatchNorm2d(planes) if batch_norm else lambda x: x
+        self.dp3 = nn.Dropout2d(p=dropout_prob)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
+        x = self.dp1(x)
 
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
+        x = self.dp2(x)
 
         x = self.conv3(x)
         x = self.bn3(x)
         x = self.relu(x)
+        x = self.dp3(x)
 
         return x
 
@@ -57,7 +63,8 @@ class NiN(ExperimentBaseModel):
         self.blocks = nn.Sequential(*blocks)
 
         self.conv = nn.Conv2d(self.base_width * width, self.dataset_type.K, kernel_size=1, stride=1)
-        self.bn = nn.BatchNorm2d(self.dataset_type.K) if batch_norm else nn.Dropout2d(p=dropout_prob)
+        self.bn = nn.BatchNorm2d(self.dataset_type.K) if batch_norm else lambda x: x
+        self.dp = nn.Dropout2d(p=dropout_prob)
         self.relu = nn.ReLU(inplace=True)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -68,6 +75,7 @@ class NiN(ExperimentBaseModel):
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
+        x = self.dp(x)
 
         x = self.avgpool(x)
 
