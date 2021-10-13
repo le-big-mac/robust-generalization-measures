7613c3b6ec59cbf8f2cade43b8a2ee96707b4db2
Charlie <charlielondon02@gmail.com>
2021-10-13 01:01:42 +0100

    log complexity measures

--- data/generation/experiment.py
+++ data/generation/experiment.py
@@ -103,8 +103,11 @@ class Experiment:
             self.state.global_batch = (self.state.epoch - 1) * len(self.train_loader) + self.state.batch
 
             if self.state.global_batch in self.config.log_steps:
-                self.evaluate_batch(DatasetSubsetType.TRAIN, True, batch_losses)
-                self.evaluate(DatasetSubsetType.TEST)
+                train_eval = self.evaluate_batch(DatasetSubsetType.TRAIN, True, batch_losses)
+                val_eval = self.evaluate(DatasetSubsetType.TEST)
+                train_eval.all_complexities[CT.VALIDATION_ACC] = val_eval.acc
+                self.logger.log_generalization_gap(self.state, train_eval.acc, val_eval.acc, train_eval.avg_loss,
+                                                   val_eval.avg_loss, train_eval.all_complexities)
             if self.state.global_batch > max(self.config.log_steps):
                 break
 
