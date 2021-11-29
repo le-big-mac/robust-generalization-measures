235753986a4e1c28e3183d5e49a57b2b2b48990a
Charlie <charlielondon02@gmail.com>
2021-11-28 23:53:28 -0500

    correctly get normal runs

--- experiments/hp_correlation/save_measures.py
+++ experiments/hp_correlation/save_measures.py
@@ -3,72 +3,68 @@ import numpy as np
 import pickle
 import math
 from typing import Tuple
-import sys
 
 api = wandb.Api()
 project_names = ["rgm", "rgm_early_batches", "rgm_dropout"]
 params = ["lr", "batch_norm", "batch_size", "model_depth", "dropout_prob"]
-config_values_dict = {"default": {'lr': [0.001, 0.00158, 0.00316, 0.00631, 0.01, 0.02, 0.05, 0.1],
-                                  'batch_size': [32, 64, 128, 256], 'model_depth': [2, 3, 4, 5], 'seed': [0, 17, 43],
-                                  'dropout_prob': [0], 'batch_norm': [True]},
-                      "dropout_prob": {'lr': [0.01], 'batch_size': [32], 'model_depth': [2, 3, 4, 5],
-                                       'seed': [0, 16, 17, 42, 43],
-                                       'dropout_prob': [0, 0.1, 0.2, 0.3, 0.4, 0.5], 'batch_norm': [True]},
-                      "batch_norm": {'lr': [0.00316, 0.00631, 0.01, 0.02], 'batch_size': [32],
-                                     'model_depth': [1, 4/3, 5/3, 2], 'seed': [0, 17, 43], 'dropout_prob': [0],
-                                     'batch_norm': [True, False]},
-                      "restricted_lr": {'lr': [0.001, 0.00316, 0.01, 0.05],
-                                        'batch_size': [32, 64, 128, 256], 'model_depth': [2, 3, 4, 5],
-                                        'seed': [0, 17, 43], 'dropout_prob': [0], 'batch_norm': [True]},
-                      }
-bad_runs = [(5, 0.3, 17), (5, 0.2, 17), (5, 0.15, 43)]
-reseed = {(5, 0.3, 42): 17, (5, 0.2, 16): 17, (5, 0.15, 42): 43}
+bad_runs = [(5, 0.3, 17), (5, 0.2, 17), (5, 0.15, 43), (5, 0.4, 43)]
+reseed = {(5, 0.3, 42): 17, (5, 0.2, 16): 17, (5, 0.15, 42): 43, (5, 0.4, 42): 43}
 
 
-def save_runs(name):
-    config_range = config_values_dict[name]
+def is_invalid_run(r):
+    return r.state == "running" or r.config["dropout_prob"] * 10 % 1 != 0 or r.config["dropout_prob"] > 0.5 or \
+           r.config["lr"] > 0.1 or r.config["lr"] < 0.001 or "batch_norm" not in r.config or not r.config["batch_norm"]
+
+
+def save_runs():
     all_runs = {}
-
     num_runs = 0
+    skipped = set()
 
     for proj_name in project_names:
         runs = api.runs(proj_name)
         for r in runs:
-            if (name == "batch_norm" and "batch_norm_layers" not in r.config) or \
-                    (name != "batch_norm" and "batch_norm_layers" in r.config):
+            if is_invalid_run(r):
                 continue
-            elif name == "batch_norm":
+
+            if "batch_norm_layers" in r.config:
                 r.config["batch_norm"] = True if len(r.config["batch_norm_layers"]) > 0 else False
                 r.config["model_depth"] = r.config["model_depth"]/3
-
-            is_valid = all(r.config[key] in config_range[key] for key in (params + ["seed"]))
-
-            if r.state == "running" or "batch_norm" not in r.config or not is_valid:
-                continue
 
             # some of the seeds got messed up in the dropout_prob experiments, fixing them here
             replace_key = (r.config["model_depth"], r.config["dropout_prob"], r.config["seed"])
             if replace_key in bad_runs:
                 continue
-            r.config["seed"] = r.config["seed"] if replace_key not in reseed else reseed[replace_key]
+            seed = r.config["seed"] = r.config["seed"] if replace_key not in reseed else reseed[replace_key]
 
             key = tuple(r.config[x] for x in params)
             if key not in all_runs:
                 all_runs[key] = []
 
             # combine early batches and normal runs
-            run = next((x for x in all_runs[key] if x.seed == r.config["seed"]), None)
-            if run is None:
-                run = Run(r)
-            else:
-                run.filter_steps(r.history(samples=10000, pandas=False))
+            run = next((x for x in all_runs[key] if x.seed == seed), None)
+            if run is None and (*key, seed) not in skipped:
+                try:
+                    run = Run(r)
+                    all_runs[key].append(run)
+                    num_runs += 1
+                except ValueError:
+                    print({x: r.config[x] for x in ["seed"] + params})
+                    skipped.add((*key, seed))
+            elif run is not None:
+                try:
+                    run.filter_steps(r.history(samples=10000, pandas=False))
+                except ValueError:
+                    print(run.seed)
+                    print(run.config)
+                    skipped.add((*key, seed))
+                    all_runs[key].remove(run)
+                    num_runs -= 1
 
-            all_runs[key].append(run)
-            num_runs += 1
+    print("Total number of runs: {}".format(num_runs))
+    print("{} runs skipped for containing invalid values".format(len(skipped)))
 
-    print(num_runs)
-
-    with open("./data/runs_{}.pickle".format(name), "wb+") as out:
+    with open("./data/runs.pickle", "wb+") as out:
         pickle.dump(all_runs, out)
 
 
@@ -101,9 +97,6 @@ class Run:
             if "complexity/SOTL" in e and step <= 1000 * self.steps_in_epoch - 1:
                 e = self.filter_measures(e)
                 if "Infinity" in e.values() or np.nan in e.values():
-                    # continue
-                    print(self.seed)
-                    print(self.config)
                     raise ValueError("Run contains invalid measure values")
 
                 epoch = self.convert_step_to_epoch(step)
@@ -148,6 +141,4 @@ class Run:
 
 
 if __name__ == "__main__":
-    names = ["default", "dropout_prob", "batch_norm", "restricted_lr"]
-    for n in names:
-        save_runs(n)
+    save_runs()
